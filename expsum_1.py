85693ed235b48415248b4200d8beb4ce91b78811
Timsbim <github@thomas-jacobi.de>
2021-08-07 16:09:25 +0200

    Update

--- expsum_1.py
+++ expsum_1.py
@@ -26,31 +26,6 @@ def calculate_vertices(year, month, day) -> np.array:
     return np.add.accumulate([vertices(n) for n in np.arange(length)])
 
 
-def calculate_vertices(year: int, month: int, day: int) -> np.array:
-    """Function calculates the vertices of the plot(s):
-
-     - The year is normalized (year = year - 2000)
-
-     - Length of the plotted sum: LCM(month, day year) + 1
-    """
-
-    # Normalizing year
-    year -= 2000
-
-    # Setting the length of the sum
-    length = np.lcm.reduce((month, day, year)) + 1
-
-    return np.add.accumulate(
-        [
-            np.exp(
-                2j * np.pi
-                * (pow(n, 1) / month + pow(n, 2) / day + pow(n, 3) / year)
-            )
-            for n in np.arange(length)
-        ]
-    )
-
-
 if __name__ == '__main__':
 
     # Base directory
