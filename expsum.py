d27753054a5ccea996cec2e6ed8022d595774dd1
Timsbim <github@thomas-jacobi.de>
2021-08-09 13:57:52 +0200

    Update

--- expsum.py
+++ expsum.py
@@ -1,26 +1,11 @@
 from pathlib import Path
 from datetime import date, timedelta
+from itertools import islice
+from multiprocessing import Pool
 
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
-
-
-def _prepare_plot(ax, x_axis, y_axis):
-    # Making sure the plot preserves the natural proportions
-    x_min, x_max = np.min(x_axis), np.max(x_axis)
-    y_min, y_max = np.min(y_axis), np.max(y_axis)
-
-    # Calculating the centre point
-    x_0 = x_min + (x_max - x_min) / 2
-    y_0 = y_min + (y_max - y_min) / 2
-
-    # Calculating the visible area around the centre point
-    half_interval = max((x_max - x_min), (y_max - y_min)) / 2
-
-    # Plotting
-    ax.set_xlim(x_0 - half_interval, x_0 + half_interval)
-    ax.set_ylim(y_0 - half_interval, y_0 + half_interval)
 
 
 def _calculate_vertices(dt_day):
@@ -40,6 +25,75 @@ def _calculate_vertices(dt_day):
     )
 
     return sums.real, sums.imag
+
+
+def _prepare_plot(ax, x_axis, y_axis):
+    # Making sure the plot preserves the natural proportions
+    x_min, x_max = np.min(x_axis), np.max(x_axis)
+    y_min, y_max = np.min(y_axis), np.max(y_axis)
+
+    # Calculating the centre point
+    x_0 = x_min + (x_max - x_min) / 2
+    y_0 = y_min + (y_max - y_min) / 2
+
+    # Calculating the visible area around the centre point
+    half_interval = max((x_max - x_min), (y_max - y_min)) / 2
+
+    # Plotting
+    ax.set_xlim(x_0 - half_interval, x_0 + half_interval)
+    ax.set_ylim(y_0 - half_interval, y_0 + half_interval)
+
+
+def _plot_args(start, end, multi):
+    days = (
+        start + timedelta(days=days) for days in range((end - start).days + 1)
+    )
+    if multi:
+        while True:
+            s = tuple(islice(days, 0, 6))
+            if len(s) > 0:
+                yield s
+            else:
+                break
+    else:
+        yield from days
+
+
+def _plot(days, path):
+
+    if isinstance(days, tuple):  # 6-packs :) of days
+        # Creating figure
+        fig = plt.figure(figsize=(10, 15))
+        for i, day in enumerate(days, start=1):
+            # Creating ax
+            ax = fig.add_subplot(3, 2, i)
+            # Calculating vertices
+            x_axis, y_axis = _calculate_vertices(day)
+            # Prepare plotting
+            _prepare_plot(ax, x_axis, y_axis)
+            # Plotting
+            ax.axis('off')
+            ax.plot(x_axis, y_axis, linewidth=1.5)
+
+        # Saving plots in file
+        file_path = path / f"{days[0]} - {days[-1]}.png"
+        plt.savefig(file_path)
+        plt.close('all')
+    else:  # Single days
+        day = days
+        fig, ax = plt.subplots(figsize=(5, 5))
+        # Calculating vertices
+        x_axis, y_axis = _calculate_vertices(day)
+        # Prepare plotting
+        _prepare_plot(ax, x_axis, y_axis)
+        # Plotting
+        ax.axis('off')
+        ax.plot(x_axis, y_axis, linewidth=1.5)
+
+        # Saving plot in file
+        file_path = path / f"{day}.png"
+        plt.savefig(file_path)
+        plt.close('all')
 
 
 class ExpSum:
@@ -69,55 +123,18 @@ class ExpSum:
         # Creating plot folder
         self._plot_path.mkdir(parents=True, exist_ok=True)
 
-        one_day = timedelta(days=1)
-        day = start
+        # Preparing the arguments for the Pool: args and number of arguments
+        args = (
+            (days, self._plot_path) for days in _plot_args(start, end, multi)
+        )
+        num_args = (end - start).days + 1
         if multi:
-            # Creating plots for 6 days at a time
-            while day <= end:
-                start_day = day
+            num_args, r = divmod(num_args, 6)
+            if r:
+                num_args += 1
 
-                # Creating figure
-                fig = plt.figure(figsize=(10, 15))
-                for i in range(1, min((end - day).days + 1, 6) + 1):
-                    # Creating ax
-                    ax = fig.add_subplot(3, 2, i)
-                    # Calculating vertices
-                    x_axis, y_axis = _calculate_vertices(day)
-                    # Prepare plotting
-                    _prepare_plot(ax, x_axis, y_axis)
-                    # Plotting
-                    ax.axis('off')
-                    ax.plot(x_axis, y_axis, linewidth=1.5)
-
-                    # Next day ...
-                    day = day + one_day
-
-                # Saving plots in file
-                file_path = self._plot_path / f"{start_day} - {day}.png"
-                plt.savefig(file_path)
-                plt.close('all')
-        else:
-            # Creating one plot for every day in the range between start and
-            # end day
-            day = start
-            while day <= end:
-                # Creating figure and ax
-                fig, ax = plt.subplots(figsize=(5, 5))
-                # Calculating vertices
-                x_axis, y_axis = _calculate_vertices(day)
-                # Prepare plotting
-                _prepare_plot(ax, x_axis, y_axis)
-                # Plotting
-                ax.axis('off')
-                ax.plot(x_axis, y_axis, linewidth=1.5)
-
-                # Saving plot in file
-                file_path = self._plot_path / f"{day}.png"
-                plt.savefig(file_path)
-                plt.close('all')
-
-                # Next day ...
-                day = day + one_day
+        with Pool(min(num_args, 12)) as p:
+            p.starmap(_plot, args)
 
     def animate(self, day=date.today(), save=False):
         if isinstance(day, str):
@@ -143,8 +160,11 @@ class ExpSum:
         plt.show()
 
 
-e = ExpSum()
-#e.plot()
-#e.plot("2021-08-01", "2021-09-30")
-#e.plot("2021-10-01", "2021-12-31", multi=True)
-e.animate(save=True)
+if __name__ == "__main__":  # Multiprocessing -- shielding the process creation
+
+    es = ExpSum()
+    es.plot()
+    es.plot("2021-08-01", "2021-08-30")
+    es.plot("2021-10-01", "2021-12-31", multi=True)
+    es.animate(save=True)
+
